2dec9309d7cfc3a9ed5731f0864e55ec1ede2f50
Aryan-Gupta2002 <ag912388@gmail.com>
2026-04-22 21:54:57 +0530

    Sign-up end point

--- Course Selling App/index.js
+++ Course Selling App/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 require("dotenv").config();
 const {mongoose, get} = require('mongoose');
-const jwt = require('jsonwebtoken');
 const { userRouter } = require('./routes/user');
 const { courseRouter } = require('./routes/course');
 const { adminRouter } = require('./routes/admin');
@@ -11,6 +10,10 @@ app.use(express.json());
 app.use("/api/v1/user",userRouter);
 app.use("/api/v1/course",courseRouter);
 app.use("/api/vi/admin",adminRouter);
+
+function authMiddleware (req,res,next){
+    
+}
 
 async function dbConnect() {
     await mongoose.connect(process.env.MONGOCON)
--- Course Selling App/routes/admin.js
+++ Course Selling App/routes/admin.js
@@ -1,22 +1,66 @@
 const {Router} = require('express');
 const adminRouter = Router();
+const {z} = require('zod');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const { adminModel } = require('./db');
 
-adminRouter.post('/signup',function(req,res){
-    res.json({
-        msg:"signup endpoint"
+adminRouter.post('/course/signup',async function(req,res){
+    // Input VAlidation Start using zod ------ 
+    const requiredInputBody = z.object({
+        email:string().email().min(3).max(30),
+        password:string().min(5).max(30),
+        f_name:string().min(3).max(30),
+        l_name:string().min(3).max(30)
     })
+    const parsedData = requiredInputBody.safeParse(req.body);
+    if(!parsedData.success){
+        res.json({
+            msg:"Input is in Incorrect format",
+            error:parsedData.error
+        })
+        return;
+    }
+    //------Input Validation End
+    const email = req.body.email;
+    const password = req.body.password;
+    const f_name = req.body.f_name;
+    const l_name = req.body.l_name;
+    let errCaught = false;
+    try{
+        //Hashing Password Start -----
+        const hashedPass = bcrypt.hash(password,5);
+        // Adding User to adminModel collection -----
+        await adminModel.create({
+            email:email,
+            password:hashedPass,
+            f_name:f_name,
+            l_name:l_name
+        })
+    }catch(e){
+        res.json({
+            msg:"User already exists"
+        })
+        errCaught = true 
+    }
+    if(!errCaught){
+        res.json({
+            msg:"You have signed up"
+        })
+    }
 });
-adminRouter.post('/signin',function(req,res){
+adminRouter.post('/course/signin',function(req,res){
+
     res.json({
             msg:"sign in endpoint"
     })
 });
-adminRouter.post('/course',function(req,res){
+adminRouter.post('/course/',function(req,res){
     res.json({
             msg:"course creation"
     })
 });
-adminRouter.put('/course',function(req,res){
+adminRouter.put('/course/',function(req,res){
     res.json({
             msg:"course update"
     })
