c8b6cab73983101b65426cd60430311e53eb9a95
Aryan-Gupta2002 <ag912388@gmail.com>
2026-04-23 12:36:18 +0530

    Updated gitignore and dotenv

---
+++ Course Selling App/middleware/user.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+const JWT_USER_SECRET = process.env.JWT_USER_SECRET;
+
+function userAuth(req,res,next){
+    const token = req.headers.token;
+    const decodedInfo = jwt.verify(token,JWT_USER_SECRET);
+    if(decodedInfo){
+        req.userID = decodedInfo.id;
+        next();
+    }else{
+        res.status(403).json({
+            msg:"Wrong Credentials"
+        })
+    }
+}
+
+module.exports={
+    userAuth:userAuth
+}   
